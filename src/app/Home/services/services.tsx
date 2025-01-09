58f77e612ff9071267fbce5fe50786e79f380d0f
ahmed123humran <ahm88m50@gmail.com>
2025-01-09 19:46:04 +0300

    [UPDASTE] home page

--- src/app/Home/services/services.tsx
+++ src/app/Home/services/services.tsx
@@ -40,7 +40,7 @@ const Services = () => {
     >
       <div className="pt-10">
         <div className="text-center text-success pb-24">
-          <h1 className="text-2xl md:text-4xl" data-aos="zoom-in">الخدمات التي نقدمها لعملائنا</h1>
+          <h1 className="text-2xl md:text-4xl" data-aos="zoom-in">خدماتنا</h1>
         </div>
         <div className="grid xl:grid-cols-4 md:grid-cols-3 grid-cols-2 gap-8 mx-20" data-aos="fade-up">
           {l?.map((l) => (
